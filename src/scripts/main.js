df1a8380f29ec15f44662f55f0035b91bdbc5dff
Olga Wukelan <olga.wukelan@gmail.com>
2025-12-05 22:32:31 +0400

    create a file Growth table DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,60 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('.container');
+const field = container.querySelector('.field');
+const tBody = field.querySelector('tbody');
+const appendRow = container.querySelector('.append-row');
+const appendColumn = container.querySelector('.append-column');
+const removeRow = container.querySelector('.remove-row');
+const removeColumn = container.querySelector('.remove-column');
+
+container.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'BUTTON') {
+    return;
+  }
+
+  const allRows = field.querySelectorAll('tr'); // Отримує колекцію всіх рядків
+  const firstRow = allRows[0]; // Отримує перший рядок
+
+  const maxCount = 10;
+  const minCount = 2;
+
+  const currentCols = firstRow ? firstRow.cells.length : 0;
+  const currentRows = allRows.length ? allRows.length : 0;
+
+  if (e.target === appendColumn) {
+    allRows.forEach((row) => {
+      const createTD = row.insertCell('td');
+      row.appendChild(createTD);
+    });
+  }
+
+  if (e.target === removeColumn) {
+    if (currentCols > minCount) {
+      allRows.forEach((row) => {
+      row.deleteCell(currentCols - 1);
+      });
+    }
+  }
+
+  if (e.target === removeRow) {
+    if (currentRows > minCount) {
+      tBody.deleteRow(currentRows - 1); // видалення останього рядку
+    }
+  }
+
+  if (e.target === appendRow) {
+    if (currentRows < maxCount) {
+      const newR = tBody.insertRow();
+
+      for (let i = 0; i < currentCols; i++) {
+        // Додаємо нову комірку в кінець (індекс не потрібен)
+        newR.insertCell(i);
+      }
+    }
+  }
+  appendRow.disabled = currentRows > maxCount;
+  appendColumn.disabled = currentCols > maxCount;
+  removeRow.disabled = currentRows < minCount;
+  removeColumn.disabled = currentCols < minCount;
+});
